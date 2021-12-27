1a02361a201afca6d3a9ff96e414adedf98bd1bd
Invitado <Invitad3.erer@gmail.com>
2021-12-27 02:41:50 +0000

    21POLITOS BORRABD TODO

--- elmundo.py
+++ elmundo.py
@@ -1,7 +1,2 @@
-Y ahora que, no hay luna no hay estrellas 
-TU nombre de la arena de la arena se boro
-Selena Gomez master cambiando
-Probabily sell mather my name is bluerryface and I care what you thino
-ook
-21pilots has changed
-TELL ME WHY I NEVED I WANT 
+ 
+HOLA
